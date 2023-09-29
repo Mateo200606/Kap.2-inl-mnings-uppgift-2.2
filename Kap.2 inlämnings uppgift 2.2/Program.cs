dd93547b8464129f189718ad9dad83dc9a5693d8
Admin <Admin@TEB123007>
2023-09-29 14:56:49 +0200

    Add project files.

---
+++ Kap.2 inlämnings uppgift 2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kap2_inlämnings_uppgift_2_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" Hur många meter hoppade Elin? ");
+           
+        
+            string x = Console.ReadLine();
+            int R1 =int.Parse(x);
+            Console.WriteLine(" Hur många meter hoppade Alma? ");
+            string y = Console.ReadLine();
+            int R2 = int.Parse(y);
+            Console.WriteLine(" Hur många fler meter hoppade Elin än Alma? ");
+            string z = Console.ReadLine();
+            int R3 = int.Parse(z);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
